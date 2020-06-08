9cfa622939b1be79e7d67d2a3bd959a49b65dab9
aborkowo <aborkowo@gmail.com>
2020-06-08 09:13:58 +0200

    generating pivot ilustration

--- src/qsorthtml.py
+++ src/qsorthtml.py
@@ -21,7 +21,7 @@ def num_row(A, p, n, ite = 0, swap = False):
             cl = 'pivot'
         elif i< n:
             cl ='left'
-        elif swap and (i == ite or i == n):
+        elif ((swap==True) and (i == ite or i == n)):
             cl = 'swap'
 
         s+= f'<td class="{cl}"> {a} </td>\n'
@@ -38,19 +38,131 @@ def next_row(A, n):
     return s
 
 
+def pivot(A, begin, end):
+    #select_pivot(A, begin, end)
+    #next smaller element
+    n = begin+1
+    p = A[begin]
+    s=''
+    
+    for i in range(begin+1, end):
+        s+=f"<p>iteration: {i} </p>"
+        s+="<table>\n"
+        s+= iter_row(A[begin:end], i)
+        if  A[i] < p :
+            s+=num_row(A[begin:end],0, n, i, True)
+            swap(A, i, n)
+            n +=1
+            s+=num_row(A[begin:end],0, n, i, False)
+        else:
+            s+=num_row(A[begin:end],0, n, i, False)
+        s+=next_row(A[begin:end], n-begin)
+        s+="</table>\n"
+
+    s+=f"<p>finally </p>"
+    s+="<table>\n"
+    s+=iter_row(A[begin:end], i-begin)
+    s+=num_row(A[begin:end],0, n-1, begin, True)
+    swap(A, begin, n-1)
+    s+=num_row(A[begin:end],n-1, n-1, begin, False)
+    s+=next_row(A[begin:end], n-begin)
+    s+="</table>\n"
+    return n-1,s
+
+html_header = ""
+html_footer = ""
+
+def swap(A, i, j):
+    tmp = A[i]
+    A[i] = A[j]
+    A[j] = tmp
+    
 #test
 
-x = [1, 100, 20, 4 ,3, 2, 5, 5, 3]
-def test():
-    s = iter_row(x, 4)
-    print(s)
-    s = num_row(x, 0, 3)
-    print(s)
-    s = num_row(x, 0, 3,5, True)
-    print(s)
-    s =next_row(x, 5)
-    print(s)
+header = """<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+ /* border: 2px solid black;
+  margin-left: 10px;
+  */
+}
+tr, {
+  /* border: 1px solid black;*/
+  text-align: left;
+  padding: 8px;
+  /*background-color: #2196F3;*/
+}
 
+td {
+  /*background-color: #2196F3;*/
+}
+
+th, td {
+  /* border: 1px solid black; */
+  width: 50px
+
+}
+.pivot {
+	background-color: lightblue;
+}
+
+.iter {
+	background-color: orange;
+
+}
+
+.next {
+	background-color: grey;
+
+}
+
+.swap {
+	background-color: yellow;
+
+}
+
+.left {
+	background-color: blue;
+
+}
+.clear {
+	background-color: white;
+}
+
+</style>
+</head>
+<body>
+<table>
+"""
+
+footer = """
+</table>
+
+</body>
+</html>
+
+"""
+
+x = [11, 100, 20, 4 ,3, 2, 5, 5, 3, 15, 6 ]
+def test():
+#    s = iter_row(x, 4)
+#    print(s)
+#    s = num_row(x, 0, 3)
+#    print(s)
+#    s = num_row(x, 0, 3,5, True)
+#    print(s)
+#    s =next_row(x, 5)
+#    print(s)
+    s1 = header
+    _, s = pivot(x, 0, len(x))
+    s1 +=s
+    s1+=footer
+    
+    print(s1)
+    with open("pivot.html", "w") as f:
+        f.write(s1)
 
 
 
